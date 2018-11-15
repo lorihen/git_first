b4f04dc713b15a6e7ebdbb8060d9cb7ab3352d3d
lorihen <lorihen@163.com>
2018-11-15 21:34:32 +0800

    把本地工程添加到git

--- git_first_lorihen/src/main/java/lorihen/git/GitFirst.java
+++ git_first_lorihen/src/main/java/lorihen/git/GitFirst.java
@@ -1,7 +1,13 @@
 package lorihen.git;
 
 public class GitFirst {
+
     public static void main(String[] args) {
         System.out.println("第一次使用idea建立git");
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
     }
 }
